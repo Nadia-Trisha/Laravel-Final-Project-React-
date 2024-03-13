0ee13629c5716484f2dad9d66a14d1a2a64512aa
Nadia-Trisha <nadiatrisha68@gmail.com>
2024-03-14 02:03:34 +0600

    laravel

--- laravel_final_react.sql
+++ laravel_final_react.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2024 at 07:03 AM
+-- Generation Time: Mar 13, 2024 at 06:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `final_project`
+-- Database: `laravel_final_react`
 --
 
 -- --------------------------------------------------------
@@ -44,7 +44,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `email_verified_at`, `password`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@gmail.com', NULL, '$2y$12$zoDW/GRPD7tHU6Wps0I38u3j2JpfXxGru8vQv92xSbrMoCwZ..zDS', 1, NULL, '2024-02-25 03:21:47', '2024-02-25 03:21:47');
+(1, 'admin', 'admin@gmail.com', NULL, '$2y$12$LdKK8a4W680ng7IAtHqOk.0uSsq1/dX5P8A9eNg1FiqdzQKGvngGC', 1, NULL, '2024-03-09 03:42:07', '2024-03-09 03:42:07');
 
 -- --------------------------------------------------------
 
@@ -79,12 +79,9 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Chocolate cake', '2024-02-25 03:21:47', '2024-02-25 03:21:47'),
-(2, 'Vanila Cake', '2024-02-25 03:21:47', '2024-02-25 03:21:47'),
-(3, 'Red Velbet', '2024-02-25 03:21:47', '2024-02-25 03:21:47'),
-(4, 'Marvel Cake', '2024-02-25 06:13:42', '2024-02-25 06:13:42'),
-(5, 'Carrot Cake', '2024-02-25 06:28:58', '2024-02-25 06:28:58'),
-(7, 'Coconut Cake.', '2024-02-27 05:31:27', '2024-02-27 05:31:27');
+(1, 'Chocolate cake', '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(2, 'Vanila Cake', '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(3, 'Red Velbet', '2024-03-09 03:42:07', '2024-03-09 03:42:07');
 
 -- --------------------------------------------------------
 
@@ -102,16 +99,6 @@ CREATE TABLE `contacts` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `contacts`
---
-
-INSERT INTO `contacts` (`id`, `name`, `email`, `phone`, `address`, `subject`, `created_at`, `updated_at`) VALUES
-(1, 'Mamunur', 'mamun@gmail.com', 1876543345, 'Vola', 'hello there is some', '2024-02-25 06:26:48', '2024-02-25 06:26:48'),
-(2, 'Nabila Naeem', 'nabila@gmail.com', 175674836, 'Demra', 'Hello ,I am Nabila. Want to know about your contact.', '2024-02-25 06:27:51', '2024-02-25 06:27:51'),
-(3, 'Nazmun  Nahar', 'nazmun@gmail.com', 12345678, 'Sylhet', 'hello there is something important to share to you', '2024-02-27 05:20:24', '2024-02-27 05:20:24'),
-(4, 'Masum', 'masum@gmail.com', 175674836, 'vh', 'hgfhg', '2024-02-27 05:39:01', '2024-02-27 05:39:01');
 
 -- --------------------------------------------------------
 
@@ -135,8 +122,7 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'shikha', 'shikha@gmail.com', NULL, '$2y$12$4WPRuatDSs5u5/FAxjNPBOI7YkqIPwVheaARk1pV8tySywTY8vvB6', NULL, NULL, NULL),
-(2, 'Rojoni', 'rojoni@gmail.com', NULL, '$2y$12$4WPRuatDSs5u5/FAxjNPBOI7YkqIPwVheaARk1pV8tySywTY8vvB6', NULL, NULL, NULL);
+(1, 'shikha', 'shikha@gmail.com', NULL, '$2y$12$YT.8EK4jQHsBHYfo7rGCPOZ.AIS/.LHtHW1z1h/NEkTYIN93t5Xkm', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -168,15 +154,6 @@ CREATE TABLE `masterchief` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `masterchief`
---
-
-INSERT INTO `masterchief` (`id`, `name`, `designation`, `image`, `created_at`, `updated_at`) VALUES
-(5, 'Alauddin', 'senior chief', '1708841177.jpg', '2024-02-25 00:06:17', '2024-02-25 00:06:17'),
-(6, 'Masum Hossain', 'senior chief', '1708841898.jpg', '2024-02-25 00:18:18', '2024-02-25 00:18:18'),
-(7, 'Najnin Zahan', 'junior chief', '1708841948.jpg', '2024-02-25 00:19:08', '2024-02-25 00:19:08');
 
 -- --------------------------------------------------------
 
@@ -221,7 +198,8 @@ CREATE TABLE `orders` (
   `total_amount` double NOT NULL,
   `productName` varchar(255) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `customerName` varchar(255) DEFAULT NULL,
+  `customerName` varchar(255) NOT NULL,
+  `customer_id` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
@@ -230,18 +208,6 @@ CREATE TABLE `orders` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `order_number`, `sub_total`, `total_amount`, `productName`, `quantity`, `customerName`, `email`, `phone`, `country`, `address`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'ORD 2131', 1600, 1600, 'Red Velbet cake', 2, '1', 'shikha@gmail.com', '0175674836', 'Bangladesh', 'Dhaka', 1, '2024-02-24 22:14:11', '2024-02-25 00:31:32'),
-(2, 'ORD 2103', 2900, 2900, 'Vanila cake', 3, '1', 'shikha@gmail.com', '01876543345', 'Bangladesh', 'Chitagong', 0, '2024-02-24 22:32:55', '2024-02-24 22:32:55'),
-(3, 'ORD 2999', 1000, 1000, 'Chocolate cake', 1, '1', 'shikha@gmail.com', '01876543345', 'Bangladesh', 'Chitagong', 0, '2024-02-25 00:25:25', '2024-02-26 22:02:53'),
-(4, 'ORD 4044', 8300, 8300, 'Red Velbet cake', 9, '1', 'shikha@gmail.com', '0175674836', 'Bangladesh', 'Chitagong', 1, '2024-02-26 21:41:32', '2024-02-26 22:01:46'),
-(5, 'ORD 3729', 1000, 1000, 'Chocolate cake', 1, '2', 'rojoni@gmail.com', '01876543345', 'Bangladesh', 'Rajbari', 1, '2024-02-26 23:10:19', '2024-02-26 23:10:39'),
-(6, 'ORD 1161', 4700, 4700, 'Vanila cake', 5, '2', 'rojoni@gmail.com', '0175674836', 'Bangladesh', 'Chitagong', 2, '2024-02-26 23:37:27', '2024-02-26 23:38:13');
 
 -- --------------------------------------------------------
 
@@ -298,12 +264,12 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `image`, `tags`, `price`, `category_id`, `availibility`, `created_at`, `updated_at`) VALUES
-(1, 'Chocolate cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'chocolate.jpg', NULL, 1000.00, 1, 1, '2024-02-25 03:21:47', '2024-02-25 03:21:47'),
-(2, 'Vanila cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'wedding1.jpg', NULL, 900.00, 2, 1, '2024-02-25 03:21:47', '2024-02-25 03:21:47'),
-(3, 'Red Velbet cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'redvelbet.jpg', NULL, 800.00, 3, 1, '2024-02-25 03:21:47', '2024-02-25 03:21:47'),
-(5, 'Marvel Cake', '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,</p>', '1708836229.jpg', '[\"lether\"]', 3500.00, 2, 0, '2024-02-24 22:43:49', '2024-02-25 00:31:09'),
-(6, 'Carrot', '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,</p>', '1708842610.jpg', '[\"full-sleeves\",\"half-sleeves\"]', 3500.00, 5, 1, '2024-02-25 00:30:10', '2024-02-26 21:38:11'),
-(8, 'Cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia', '1709011959.jpg', '[\"full-sleeves\",\"half-sleeves\",\"lether\"]', 564.00, 7, 1, '2024-02-26 23:32:39', '2024-02-26 23:32:39');
+(1, 'Chocolate cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'chocolate.jpg', NULL, 1000.00, 1, 1, '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(2, 'Vanila cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'wedding1.jpg', NULL, 900.00, 2, 1, '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(3, 'Red Velbet cake', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'redvelbet.jpg', NULL, 800.00, 3, 1, '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(4, 'Chocolate cake 2', ' This is a birthday cake,Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'chocolate.jpg', NULL, 1000.00, 1, 1, '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(5, 'Vanila cake 2', 'This is vanila cake. Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'wedding1.jpg', NULL, 400.00, 2, 1, '2024-03-09 03:42:07', '2024-03-09 03:42:07'),
+(6, 'Red Velbet cake 2', 'This is a cake for velbet 2 .\r\nLorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,', 'redvelbet.jpg', NULL, 500.00, 3, 1, '2024-03-09 03:42:07', '2024-03-09 03:42:07');
 
 -- --------------------------------------------------------
 
@@ -327,7 +293,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Masum', 'masum@gmail.com', NULL, '$2y$12$JT1K1UEwkikZkvlplwJcTuvI34qPVlBlHsCqpUtd.M992z9XOmoEC', NULL, '2024-02-25 03:21:46', '2024-02-25 03:21:46');
+(1, 'Nadia Trisha', 'nadia@gmail.com', NULL, '$2y$12$DIzZ9yRkBe8WejA6NmrL8uTxbvNo6vXNC7s9LVHf6JzWlx3SZJXEi', NULL, '2024-03-09 03:42:06', '2024-03-09 03:42:06');
 
 --
 -- Indexes for dumped tables
@@ -438,19 +404,19 @@ ALTER TABLE `brands`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -462,7 +428,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `masterchief`
 --
 ALTER TABLE `masterchief`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -474,7 +440,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -486,7 +452,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
